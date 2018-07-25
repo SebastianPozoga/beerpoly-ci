5633f14f26a13fed0992be50118bfa0d7ac6fc1c
Sebastian Pożoga <prog255@gmail.com>
2018-07-25 21:11:43 +0000

    correct serve static directories ports

--- beerpoly-entrypoint.sh
+++ beerpoly-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-mkdir -p serve/beerpoly
+mkdir -p data/serve/beerpoly
+mkdir -p data/serve/beerpoly-home
+mkdir -p data/archive
 ran -p=8081 -r=data/serve/beerpoly -l=true &
 ran -p=8082 -r=data/serve/beerpoly-home -l=true &
-ran -p=8082 -r=data/archive -l=true &
+ran -p=8083 -r=data/archive -l=true &
 
 sh -x "/go/src/github.com/goatcms/webslots/docker/entrypoint.sh"
